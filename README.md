19b3f51a20813d310de1e590adba6d143c8dc036
Dulanka Sheshan <dulankasheshan@gmail.com>
2023-12-04 18:48:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# schooligo_driver
+ This application help to manage transpotation of the school bus driver
---
+++ android/app/src/main/kotlin/com/schooligo/schooligo_driver/schooligo_driver/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.schooligo.schooligo_driver.schooligo_driver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
